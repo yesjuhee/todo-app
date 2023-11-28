fbc7094f78008041ec43f75524339db6a4cc1cbc
yesjuhee <juhee0924@g.skku.edu>
2023-11-28 15:09:47 +0900

    feat: 페이지 레이아웃 정리

--- js/background.js
+++ js/background.js
@@ -2,8 +2,10 @@ const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
+const main = document.querySelector("#right");
+
 // const bgImage = document.createElement("img");
 // bgImage.src = `img/${chosenImage}`;
 // document.body.appendChild(bgImage);
 
-document.body.style.backgroundImage = `url(img/${chosenImage})`;
+main.style.backgroundImage = `url(img/${chosenImage})`;
